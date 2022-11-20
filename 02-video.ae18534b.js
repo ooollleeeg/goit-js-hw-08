71214d1a0cf03c75d4200723379a9cc8cfdbe19b
ooollleeeg <93201549+ooollleeeg@users.noreply.github.com>
2022-11-20 12:47:40 +0000

    Deploying to gh-pages from @ ooollleeeg/goit-js-hw-08@922ceb86c3df943f068493ec2b1ac85ca43b21d8 🚀

---
+++ 02-video.ae18534b.js
@@ -0,0 +1,2 @@
+player.on("timeupdate",(function(e,n,t){}));
+//# sourceMappingURL=02-video.ae18534b.js.map
---
+++ 02-video.e6a3e1d4.js
@@ -0,0 +1,2 @@
+player.on("timeupdate",(function(e,n,t){}));
+//# sourceMappingURL=02-video.e6a3e1d4.js.map
