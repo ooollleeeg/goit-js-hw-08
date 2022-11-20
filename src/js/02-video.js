195e0173a254f35a1543ba5e74db7c1932c4ddb5
Oleh Honchar <ooollleeeg@gmail.com>
2022-11-20 22:03:55 +0200

    add task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,26 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-player.on('timeupdate', function (duration, percent, seconds) {
-  // data is an object containing properties specific to that event
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    const time = data.seconds;
+    localStorage.setItem('videoplayer-current-time', time);
+  }, 1000)
+);
+const onTime = localStorage.getItem('videoplayer-current-time');
+
+player
+  .setCurrentTime(onTime)
+  .then(function (seconds) {})
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+      default:
+        break;
+    }
+  });
